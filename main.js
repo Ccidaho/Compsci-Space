cb59cfe16a42737bc1a0424dc8e5ee2b1c73ce96
JMurp0817 <jmurp0817@gmail.com>
2017-02-21 08:56:01 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -3,16 +3,18 @@ function enterBridge() {
 	if (powerOn.toggle == 0) {
 	
 	description = 
-		"You find yourself on a dimly lit spaceship and can see a small sign hanging above an "+
-		"entrance to another room. "+
-		"You can just barely make out the sign, it says '<a href='#' onclick='enterEngine()'>Engine Room</a>'.";
+		"<p>You find yourself on a dimly lit spaceship and can see a small sign hanging above an "+
+		"entrance to another room.</p> "+
+		"<p>You can just barely make out the sign, it says "+
+		"'<a href='#' onclick='enterEngine()'>Engine Room</a>'.</p>";
 	} else {
 	
 	description = 
-		"The spaceship is now brightly lit up and you can see other entrances, "+
-		"back to the <a href='#' onclick='enterEngine()'>engine room</a>, "+
-		"another entrance to the <a href='#' onclick='placeholder()'>locker room</a> and one more that leads to the "+
-		"<a href='#' onclick='placeholder()'>escape pod bay</a>";
+		"<p>The spaceship is now brightly lit up and you can see other entrances, </p>"+
+		"<p>back to the <a href='#' onclick='enterEngine()'>engine room</a>, "+
+		"another entrance to the <a href='#' onclick='lckrRoom(1)'>locker room</a></p> "+
+		"<p>and one more that leads to the "+
+		"<a href='#' onclick='enterLightPod()'>escape pod bay</a></p>";
 	}
 		roomDesc = document.getElementById('RoomDesc');
 		roomDesc.innerHTML = description;
@@ -23,8 +25,8 @@ function enterBridge() {
 function enterEngine() {
 
 	description = 
-		"You enter the engine room and find a button that turns on the <a href='#' onclick='powerOn.press()'>emergency power</a>. "+
-		"There is only one exit in this room, back to the <a href='#' onclick='enterBridge()'>bridge</a>";
+		"<p>You enter the engine room and find a button that turns on the <a href='#' onclick='powerOn.press()'>emergency power</a>.</p> "+
+		"<p>There is only one exit in this room, back to the <a href='#' onclick='enterBridge()'>bridge</a></p>";
 		
 	roomDesc = document.getElementById('RoomDesc');
 	roomDesc.innerHTML = description;
@@ -37,7 +39,36 @@ var powerOn = {
 	
 	press: function() {
 	
-		powerOn.toggle = 1,
+		
+		if(powerOn.toggle == 1){
+		
+			description = 
+				"<p>You already turned on the emergency power.</p>";
+		roomDesc = document.getElementById('RoomDesc');
+		roomDesc.style.color = 'white';
+		body = document.getElementById('body');
+		body.style.backgroundColor = 'black';
+		
+		}
+		if(powerOn.toggle == 0) {
+		
+			description = 
+				"<p>You turned on the power! Now you can go check out the other rooms</p>";
+				
+			roomDesc = document.getElementById('RoomDesc');
+			roomDesc.style.color = 'black';
+			body = document.getElementById('body');
+			body.style.backgroundColor = 'white';
+			powerOn.toggle = 1
+			
+			
+		
+		}
+		roomDesc = document.getElementById('RoomDesc');
+		roomDesc.style.color = 'black';
+		body = document.getElementById('body');
+		body.style.backgroundColor = 'white';
+		roomDesc.innerHTML += description;
 		
 		console.log(powerOn.toggle)
 	
@@ -49,4 +80,134 @@ function placeholder() {
 
 	console.log('this has not been implemented yet');
 
+}
+
+//kode 4 lkr rum
+
+var curInv =[];
+var posInv =["Laser-Tool", "Holy-Bible"];
+
+function lckrRoom(input) {
+if(curInv[0] == posInv[0]) {input ++;}
+if (input == 1) {
+	description = "<p>As you enter the locker room, you can see</p>" +
+	"<p> many lockers, each of which are unlocked due to the manual power</p>" +
+	"<p> reset which occured when you turned the power back on. </p>" +
+	"<br><p><a href='#' onclick='lckrOpen()'>Crack Open A Locker</a></p>" +
+	"<br><p>Or you could head back to the <a onclick='enterBridge()'>Bridge</a></p>";
+var roomRan = document.getElementById('RoomDesc');
+roomRan.innerHTML = description;}
+if (input == 2) {
+	description = "<p>As you enter the locker room, you can see</p>" +
+	"<p> many lockers, each of which are unlocked due to the manual power</p>" +
+	"<p> reset which occured when you turned the power back on. </p>" +
+	"<br><p><a href='#' onclick='enterBridge()'>Bridge</a></p>";
+var roomRan = document.getElementById('RoomDesc');
+roomRan.innerHTML = description;
+}
+}
+
+function lckrOpen() {
+	var tmpRan = Math.floor(Math.random() * 5);
+	if(tmpRan == 0){
+		description = "<p>As you enter the locker room, you can see</p>" +
+		"<p> many lockers, each of which are unlocked due to the manual power</p>" +
+		"<p> reset which occured when you turned the power back on. </p>" +
+		"<br><p><a href='#' onclick='enterBridge()'>Bridge</a></p>";
+		description += "<p>You managed to get the laser tool!</p>"
+
+	curInv.unshift(posInv[0]);
+	
+	document.getElementById('allInv').innerHTML = laserTool.menuCreate();
+	console.log(curInv);
+
+	var roomRan = document.getElementById('RoomDesc');
+	roomRan.innerHTML = description;
+
+}else{
+
+	var roomRan = document.getElementById('RoomDesc');
+	roomRan.innerHTML += "<p>This locker had nothing inside of it.</p>"
+
+}
+}
+
+var laserTool = {
+	location:'locker',
+		menuCreate: function () {
+		menu = 
+			"<span id='laserLinkId' class='popup'>" +
+				"<a href='#' onclick='laserTool.menuDisplay()'>" +
+					"Laser-Tool" +
+				"</a>" +
+				"<span id='laserMenuId' class='popuptext'>" +
+					"<u onclick='laserTool.examine()'>" +
+						"Examine" +
+					"</u>";
+		
+		return(menu)
+		},
+		
+		menuDisplay: function () {
+		popup = document.getElementById('laserMenuId');
+		popup.style.visibility = 'visible';
+		},
+		examine: function () {
+		
+		document.getElementById('RoomDesc').innerHTML += "<p>You examine the laser tool and" +
+		" realize that it's a laser tool.</p>"
+		
+		popup = document.getElementById('laserMenuId');
+		popup.style.visibility = 'hidden';
+		},
+
+
+}
+
+/* ESCAPE POD */
+function enterLightPod() {
+console.log('enterLightPod')
+	var description =
+		"<p>You enter the escape pod bay and find one escape pod that hasn't been launched yet </p>"+
+		"<p>The escape pod's engine is not functioning, " +
+		"there might be a way to " +
+		"<a href='#' onclick='fixPod()'>Salamaamamai</a>" +
+		" it...</p>";
+		
+	var roomRan = document.getElementById('RoomDesc');
+	roomRan.innerHTML = description;
+}
+
+/* FIX POD */
+function fixPod() {
+console.log('fixPod')
+	if(curInv[0] == posInv[0]) {
+		endGame();
+	}
+	else {
+		enterRejection();
+	}
+}
+
+/* REJECTED */
+function enterRejection() {
+console.log('enterRejection')
+	var description =
+		"<p>You need a tuewul it might be on the " +
+		"<a href='#' onclick='enterBridge()'>bridge</a>." +
+		"</p>";
+		var roomRan = document.getElementById('RoomDesc');
+	roomRan.innerHTML = description;
+}
+
+/* END GAME */
+function endGame() {
+console.log('endGame')
+	var description =
+		"<p>The door salamied shut behind you, the pod " +
+		"has started to shake...<br>" +
+		"You were lanched back to earth.</p>"+
+		"<p>You Win</p>";
+		var roomRan = document.getElementById('RoomDesc');
+	roomRan.innerHTML = description;
 }
